3ecd0db7bdc1c752ae889c66668a075b31add2a1
marschla <marschla@student.ethz.ch>
2020-04-16 10:52:23 +0200

    changed some naming conventions, added structure for cascade PID

--- launch.sh
+++ launch.sh
@@ -4,5 +4,5 @@ set -e
 
 # YOUR CODE BELOW THIS LINE
 # ----------------------------------------------------------------------------
-roslaunch my_package node.launch
-#rosrun my_package sub.py
+#roslaunch my_package node1.launch
+roslaunch my_package node2.launch
--- packages/my_package/src/cascade_PID.py
+++ packages/my_package/src/cascade_PID.py
@@ -26,13 +26,12 @@ class MyNode(DTROS):
         self.vref = 0.22    #v_ref defines speed at which the robot moves 
         self.dist = 0.0
         self.dold = 0.0
-        self.tist = 0.0
+        self.phiist = 0.0
+        self.phiref 
+        self.phiest = 0.0
 
         #params used for PID control 
-        self.C_p = 0.0
-        self.C_i = 0.0
-        self.C_d = 0.0
-        self.arr_d = np.array([0.0,0.0,0.0])
+
 
         #structural paramters of duckiebot
         self.L = 0.05      #length from point A to wheels [m]
@@ -43,74 +42,56 @@ class MyNode(DTROS):
         val = np.median(data)
         return val 
 
-    def getomega(self,dist,tist,dt):
-        #parameters for PID control
-        k_p = 4.5
-        k_i = 0.9
-        k_d = 0.1
-        #saturation params
-        sati = 1.0
-        satd = 1.0
-        omegasat=5.0
-        
-        '''
-        #array for moving average filter
-        for i in range(1,3):
-            self.arr_d[i-1]=self.arr_d[i]
-        self.arr_d[2] = 6*dist+tist    #error is weighted sum of distance to lane center and heading error
+    #control alg. for inner loop
+    def getomega(self,phiref,phiist,dt):
 
-        #getting the filtered error value
-        derr = self.filter(self.arr_d)
-        '''
+        #PID params for inner loop
+        k_p = 1.0
+        k_i = 0.0
+        k_d = 0.0
+    
+        err = phiref-phiist
 
-        err = 6*dist+tist
+        omega = k_p*err
 
-        #proportional gain part
-        self.C_p = k_p*err
+        self.phiest += omega*dt
 
-        #integral term (approximate integral)
-        self.C_i += k_i*dt*err
-        
-        #make sure integral term doesnt become too big
-        if self.C_i > sati:
-            self.C_i = sati
-        if self.C_i < -sati:
-            self.C_i = -sati
-        
-        #derivative term
-        self.C_d = k_d*(err-self.dold)/dt
-        self.dold = err
-        
-        #make sure derivative term doesnt become too big
-        if self.C_d > satd:
-            self.C_d = satd
-        if self.C_d < -satd:
-            self.C_d = -satd
-        
-        #computing control output
-        omega = self.C_p + self.C_i + self.C_d
-        
-        
-        if omega>omegasat:
-            omega=omegasat
-        if omega<-omegasat:
-            omega=-omegasat
-        
         return omega
+
+    def getphiref(self,dist)
+
+        #PID params for outer loop
+        k_p = 2.0
+        k_i = 0.0
+        k_d = 0.0
+        sat = np.pi/2.0
+
+        err = dist
+
+        phiref = k_p*err
+
+        #saturation, currently at 90° (needs testing if bigger angles needed (especially in tight turns))
+        if phiref>sat:
+            phiref=sat
+        if phiref<-sat:
+            phiref=-sat
+
+        return phiref
 
     def run(self):
         # publish message every 1/x second
+        #for cascade choose rate > rate of camera
         rate = rospy.Rate(10) 
         car_cmd_msg = WheelsCmdStamped()
         tnew = time.time()
         while not rospy.is_shutdown():
-            #computing dt for I-part of controller
+            #computing dt for I and D-part of controller
             told = tnew
             tnew = time.time()
             dt = tnew-told
 
-            #self.vdiff = self.getvdiff(self.dist,self.tist,dt)
-            self.omega = self.getomega(self.dist,self.tist,dt)
+            self.phiref = self.getphiref(self.dist)
+            self.omega = self.getomega(self.phiref,self.phiist,dt)
 
             
 
@@ -119,17 +100,14 @@ class MyNode(DTROS):
             car_cmd_msg.vel_left = self.vref + self.L * self.omega
             car_cmd_msg.vel_right = self.vref - self.L * self.omega
 
-            self.pub_wheels_cmd.publish(car_cmd_msg)
+            #self.pub_wheels_cmd.publish(car_cmd_msg)
 
             #printing messages to verify that program is working correctly 
             #i.ei if dist and tist are always zero, then there is probably no data from the lan_pose
             message1 = self.dist
             message2 = self.omega
-            message3 = self.tist
+            message3 = self.phiist
             message4 = dt
-
-            if dt<0.08:
-                rospy.logwarn('dt: %s' % message4)
 
             rospy.loginfo('d: %s' % message1)
             rospy.loginfo('phi: %s' % message3)
@@ -152,7 +130,9 @@ class MyNode(DTROS):
     #thus we do not use all incoming data
     def control(self,pose):
         self.dist = pose.d
-        self.tist = pose.phi
+        self.phiist = pose.phi
+        #updating our estimate of phi
+        self.phiest = pose.phi
         #message = pose.d
         #rospy.loginfo('d =  %s' % message)
 
