089710436e5a7ebb721a5dc2e73611e47c58c5bb
marschla <marschla@student.ethz.ch>
2020-05-26 20:52:54 +0200

    first attempt at pure pursuit controller

--- packages/my_package/src/pure_pursuit.py
+++ packages/my_package/src/pure_pursuit.py
@@ -23,8 +23,8 @@ class MyNode(DTROS):
 
         # Subscriptions
         #self.sub_lane_reading = rospy.Subscriber("/autobot17/lane_filter_node/seglist_filtered", SegmentList, self.process_segments, queue_size=1)
-        self.sub_pose = rospy.Subscriber(str(os.environ['VEHICLE_NAME'])+"/line_detector_node/segment_list", SegmentList, self.process_segments, queue_size=1)
-
+        #self.sub_pose = rospy.Subscriber(str(os.environ['VEHICLE_NAME'])+"/line_detector_node/segment_list", SegmentList, self.process_segments, queue_size=1)      
+        self.sub_pose = rospy.Subscriber("/autobot17/ground_projection/lineseglist_out", SegmentList, self.process_segments, queue_size=1)
         # Publication
         #self.pub_car_cmd = rospy.Publisher("/{}/joy_mapper_node/car_cmd".format(location), Twist2DStamped, queue_size=1)
         self.pub_wheels_cmd = self.publisher(str(os.environ['VEHICLE_NAME'])+"/wheels_driver_node/wheels_cmd", WheelsCmdStamped, queue_size=1)
@@ -47,115 +47,51 @@ class MyNode(DTROS):
     def process_segments(self, input_segment_list):
         all_segments = input_segment_list.segments # this is a list of type Segment
 
-        num_white = 0
+        far = 0.4
+        tol = 0.05
+
         num_yellow = 0
-        num_white_far = 0
-        num_yellow_far = 0
-        '''
-        total_white_x = 0
-        total_white_y = 0
-        total_yellow_x = 0
-        total_yellow_y = 0
-        total_white_far_x = 0
-        total_white_far_y = 0
-        total_yellow_far_x = 0
-        total_yellow_far_y = 0
-        '''
+        num_white = 0
 
-        total_white = np.zeros(2)
-        total_white_far = np.zeros(2)
-        total_yellow = np.zeros(2)
-        total_yellow_far = np.zeros(2)
+        yellow_arr = np.zeros(2)
+        white_arr = np.zeros(2)
 
-        far = 0.5
+        rospy.loginfo("Hallo")
 
         for segment in all_segments:
-
             point0 = segment.points[0]
             point1 = segment.points[1]
-            col = segment.color
 
-            # We only care about the average point of the segment
-            ave_point = np.array([point0.x + point1.x, point0.y + point1.y]) / 2.
-            # ^ Q: is this misguided? Will this unfairly remove points?
+            ave_point_x = (point0.x + point1.x)/2.0
+            ave_point_y = (point0.y + point1.y)/2.0
 
-            if col == 0:
-                num_white += 1
-                total_white += ave_point
+            d = np.sqrt(ave_point_x**2 + ave_point_y**2)
 
-                if ave_point.dot(ave_point) > far:
-                    num_white_far += 1
-                    total_white_far += ave_point
+            if segment.color == 1:    #yellow color
+                if d < far + tol and d > far - tol:
+                    num_yellow += 1
+                    yellow_arr += np.array([ave_point_x,ave_point_y])
 
-            elif col == 1:
-                num_yellow += 1
-                total_yellow += ave_point
+            if segment.color == 0:     #white color
+                if d < far + tol and d > far - tol:
+                    num_white += 1
+                    white_arr += np.array([ave_point_x,ave_point_y])
 
-                if ave_point.dot(ave_point) > far:
-                    num_white_far += 1
-                    total_yellow_far += ave_point
+        if num_white != 0 and num_yellow != 0:
+            ave_white = white_arr * 1. / num_white
+            ave_yellow = yellow_arr * 1. / num_yellow
 
-        #ave_white_far = total_white_far * 1. / num_white_far
-        #ave_yellow_far = total_yellow_far * 1. / num_yellow_far
+            ave_point = (ave_white + ave_yellow)/2.0
 
-        # Tell the car to do based on what we see
-        car_control_msg = Twist2DStamped()
-        car_control_msg.v = 0.0
-        car_control_msg.omega = 0.0
+            alpha = np.arctan2(ave_point[1],ave_point[0])
 
-        if num_white + num_yellow == 0:
-            # Want to turn right (improve later)
-            car_control_msg.v = 0.2/2.0
-            car_control_msg.omega = -3.0
+            self.omega = 8 * np.sin(alpha)
 
-        elif num_white == 0: # and num_yellow != 0
-            if num_yellow_far > 0:
-                ave_yellow = total_yellow_far * 1. / num_yellow_far
-                ave_yellow[1] -= 0.15 # subtract offset
-            else:
-                ave_yellow = total_yellow * 1. / num_yellow
-                ave_yellow[1] -= 0.25 # subtract offset
+            rospy.loginfo("target: %s" % self.omega)
 
-            alpha = np.arctan2(ave_yellow[1], ave_yellow[0])
-            omega = 6 * np.sin(alpha)
-
-            car_control_msg.v = 0.25/2.0
-            car_control_msg.omega = omega
-
-        elif num_yellow == 0: # and num_white != 0
-            if num_white_far > 0:
-                ave_white = total_white_far * 1. / num_white_far
-                ave_white += 0.15 # add offset
-            else:
-                ave_white = total_white * 1. / num_white
-                ave_white += 0.25 # add offset
-
-            alpha = np.arctan2(ave_white[1], ave_white[0])
-            omega = 6 * np.sin(alpha)
-
-            car_control_msg.v = 0.25/2.0
-            car_control_msg.omega = omega
-
-        else: # see both colours
-            if num_white_far > 0 and num_yellow_far > 0:
-                ave_white = total_white_far * 1. / num_white_far
-                ave_yellow = total_yellow_far * 1. / num_yellow_far
-            else:
-                ave_white = total_white * 1. / num_white
-                ave_yellow = total_yellow * 1. / num_yellow
-
-            overall_ave = (ave_white + ave_yellow) / 2.
-
-            alpha = np.arctan2(overall_ave[1], overall_ave[0])
-            omega = 3 * np.sin(alpha)
-
-            car_control_msg.v = 0.4/2.0
-            car_control_msg.omega = omega
-
-        self.vref = car_control_msg.v
-        self.omega = car_control_msg.omega
-
-        
+            
+        self.vref = 0.2
+            
 
     
     def run(self):
@@ -169,11 +105,13 @@ class MyNode(DTROS):
             car_cmd_msg.vel_left = self.vref - self.L * self.omega
             car_cmd_msg.vel_right = self.vref + self.L * self.omega
 
+            '''
             msg1 = self.omega
             msg2 = self.vref
 
             rospy.loginfo("omega: %s" % msg1)
             rospy.loginfo("vref: %s" % msg2)
+            '''
 
             # Send the command to the car
             #self.pub_car_cmd.publish(car_control_msg)
@@ -185,6 +123,9 @@ if __name__ == '__main__':
     # create the node
     node = MyNode(node_name='my_node')
     # run node
+
+    #rospy.loginfo("Hallo!?!")
+
     node.run()
     # keep spinning
     rospy.spin()
